f881c411a36d4baed604d325c3ed545067e8e07b
marcinkozaczyk <87424520+marcinkozaczyk@users.noreply.github.com>
2022-06-16 13:54:02 +0200

    fix for config references

--- k8s/run-stack.ps1
+++ k8s/run-stack.ps1
@@ -1,3 +1,4 @@
 ﻿kubectl apply -f ./mamma-secret.yml -f ./kafka-pvc.yml -f ./mongo-pvc.yml -f ./rabbitmq-pvc.yml -f ./keycloak-pvc.yml -n $args[0]
 kubectl apply -f ./camunda.yml -f ./fiware.yml -f ./kafka.yml -f ./keycloak.yml -f ./mongo.yml -f ./rabbitmq.yml -n $args[0]
-kubectl apply -f ./mamma-api.yml -f ./mamma-client.yml -f ./mamma-operators-terminal.yml -f ./mamma-kafka-stream.yml -n $args[0]
+kubectl apply -f ./mamma-api.yml -f ./mamma-kafka-stream.yml -n $args[0]
+kubectl apply -f ./mamma-client.yml -f ./mamma-operators-terminal.yml -n $args[0]
--- k8s/run-stack.sh
+++ k8s/run-stack.sh
@@ -1,4 +1,5 @@
 ﻿#!/usr/bin/env bash
 kubectl apply -f ./mamma-secret.yml -f ./kafka-pvc.yml -f ./mongo-pvc.yml -f ./rabbitmq-pvc.yml -f ./keycloak-pvc.yml -n $1
 kubectl apply -f ./camunda.yml -f ./fiware.yml -f ./kafka.yml -f ./keycloak.yml -f ./mongo.yml -f ./rabbitmq.yml -n $1
-kubectl apply -f ./mamma-api.yml -f ./mamma-client.yml -f ./mamma-operators-terminal.yml -f ./mamma-kafka-stream.yml -n $1
+kubectl apply -f ./mamma-api.yml -f ./mamma-kafka-stream.yml -n $1
+kubectl apply -f ./mamma-client.yml -f ./mamma-operators-terminal.yml -n $1
